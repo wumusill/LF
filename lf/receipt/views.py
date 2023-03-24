693976d1beb14013bf10fa54af88f145729b82ce
JaHyeon_Gu <jahyungu@naver.com>
2023-03-25 00:16:35 +0900

    create LF repo & start project with django

---
+++ lf/receipt/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+import os, json
+
+# Create your views here.
+# ocr 결과물을 json으로 받는 함수
+def ocr(request):
+    return None
+
+# json파일에서 날짜, 장소, 지불 가격 3가지 정보 추출
+def to_json(request):
+    return None
